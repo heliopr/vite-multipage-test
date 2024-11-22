3855414c59786beb92f2874f0f746387b3927182
heliopr <helioprorep@gmail.com>
2024-11-21 22:59:24 -0300

    fjjsijsi

--- src/global.ts
+++ src/global.ts
@@ -3,7 +3,7 @@ import footer from "/components/footer.html?raw"
 
 console.log("global")
 
-document.querySelector("header")!.innerHTML = header
+document.querySelector("header")!.innerHTML = header.replace(new RegExp("%BASE_URL%", "g"), import.meta.env.BASE_URL)
 document.querySelector("footer")!.innerHTML = footer
 
 export default function DoSomething() {
